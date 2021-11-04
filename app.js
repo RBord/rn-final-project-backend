9ddea604a02dc3d49c26dd212d69002aef6571b5
OksanaStezhko <oksana_stegko@ukr.net>
2021-11-04 13:34:24 +0200

    шаблон

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.send('<h2>Главная страница</h2>')
+})
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found!' })
+})
+
+app.use((error, req, res, next) => {
+  const { status = 500, message = 'Server error' } = error
+  res.status(status).json({ message })
+})
+
+module.exports = app
